cd4884fe360b0c230563fa02ef14376044f0fea1
Jose Herrera <jferhc@gmail.com>
2022-03-04 11:51:46 -0500

    add setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import BookIndex from './components/BookIndex';
+import Categories from './components/Categories';
+
+const App = () => (
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<BookIndex />} />
+      <Route path="categories" element={<Categories />} />
+    </Routes>
+  </BrowserRouter>
+); 
+
+export default App;
---
+++ src/components/BookIndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Book from './Book';
+import BookForm from './BookForm';
+import './Book.css';
+
+const BookCreate = () => (
+  <>
+    <Book />
+    <BookForm />
+  </>
+);
+
+export default BookCreate;
